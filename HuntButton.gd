2262266648686f1f7be632eaf3e9c931a0dc0e50
yoshg <shua.garrett@gmail.com>
2025-02-19 16:01:00 -0500

    changes

--- HuntButton.gd
+++ HuntButton.gd
@@ -1,20 +1,18 @@
-extends Button  # Make sure the script extends Button
+extends Button
 
 @onready var cooldown_timer = $HuntCooldownTimer
-@onready var hud = get_node("/root/Main/HUD/MainHbox")  # Reference HUD
+@onready var hud = get_node("/root/Main/HUD/MainHbox")
 @onready var gold_label = hud.get_node("MainVbox/GoldLabel")
 @onready var xp_label = hud.get_node("MainVbox/XPLabel")
 @onready var result_display = get_node("/root/Main/HUD/HuntResultDisplay")
 
+var countdown = 5
 var is_on_cooldown = false
 
 func _ready():
 	connect("pressed", Callable(self, "_on_HuntButton_pressed"))
-	cooldown_timer.timeout.connect(_on_Cooldown_ended)
-	update_ui()
-	# Set the initial appearance
+	cooldown_timer.timeout.connect(Callable(self, "_on_Cooldown_ended"))
 	text = "Hunt"
-	modulate = Color(1, 1, 1)  # Normal white (or default)
 
 func _on_HuntButton_pressed():
 	if is_on_cooldown:
@@ -22,23 +20,30 @@ func _on_HuntButton_pressed():
 		return
 
 	text = "Hunting..."
-	modulate = Color(0.7,0.7,0.7)
-	
-	# Call the GameManager's hunt function
+	modulate = Color(0.7, 0.7, 0.7)
+
 	var result = GameManager.hunt()
-	
 	if result_display:
 		result_display.show_result(result)
-		result_display.show()  # Make sure it is visible
+		result_display.show()
 	else:
 		print("ERROR: HuntResultDisplay was freed!")
-		
-	
 
 	# Start cooldown
 	is_on_cooldown = true
 	disabled = true
+	countdown = int(cooldown_timer.wait_time)
+	update_button_text()
 	cooldown_timer.start()
+	_start_countdown()
+
+func _start_countdown():
+	while countdown > 0:
+		await get_tree().create_timer(1.0).timeout
+		countdown -= 1
+		update_button_text()
+
+	_on_Cooldown_ended()
 
 func _on_Cooldown_ended():
 	is_on_cooldown = false
@@ -48,6 +53,5 @@ func _on_Cooldown_ended():
 	print("Hunt is ready again!")
 	GameManager.emit_signal("hunt_reset")
 
-func update_ui():
-	gold_label.text = "Gold: " + str(GameManager.player_gold)
-	xp_label.text = "XP: " + str(GameManager.player_experience)
+func update_button_text():
+	text = "0:%02d" % countdown
--- HuntResultDisplay.gd
+++ HuntResultDisplay.gd
@@ -6,26 +6,31 @@ extends Panel
 @onready var enemy_graphic = get_node("MainVbox/EnemyGraphic")  # TextureRect for the enemy image
 @onready var gold_label = get_node("MainVbox/GoldXPContainer/GoldLabel")
 @onready var xp_label = get_node("MainVbox/GoldXPContainer/XPLabel")
+@onready var close_button = get_node(".")
 
 func _ready():
 	hide()
 	# Connect to the hunt_reset signal
 	GameManager.connect("hunt_reset", Callable(self, "_on_hunt_reset"))
+	close_button.connect("pressed", Callable(self, "_on_close_pressed"))
 	# Set custom colors
 
 func show_result(result: Dictionary) -> void:
 	enemy_label.text = "Encountered: " + result.enemy_name
 	stats_label.text = "%s Power: %d | Defense: %d" % [result.enemy_name, result.enemy_power, result.enemy_defense]
-	
+
 	if result.enemy_defeated and not result.player_defeated:
-		rewards_label.text = "Victory! Gained %d Gold and %d XP" % [result.gold_drop, result.xp_drop]
+			rewards_label.text = "Victory! Gained %d Gold and %d XP" % [result.gold_drop, result.xp_drop]
 	elif result.player_defeated and not result.enemy_defeated:
 		rewards_label.text = "Defeat! You were overwhelmed."
 	elif result.enemy_defeated and result.player_defeated:
-		rewards_label.text = "Both perished in battle..."
+			rewards_label.text = "Both perished in battle..."
 	else:
 		rewards_label.text = "The battle was inconclusive."
 
+	show()  # Make sure the result panel is visible
+	enemy_graphic.texture = null
+	
 	# Set the enemy graphic (assuming GameManager has the enemy_images dictionary)
 	if result.enemy_name in GameManager.enemy_images:
 		enemy_graphic.texture = GameManager.enemy_images[result.enemy_name]
@@ -34,5 +39,11 @@ func show_result(result: Dictionary) -> void:
 		enemy_graphic.texture = null
 
 func _on_hunt_reset() -> void:
-	
 	hide()  # Hides the panel instead of deleting it
+	
+func _on_close_pressed() -> void:
+	hide()  # Allows the player to close it manually
+
+
+func _on_close_button_pressed() -> void:
+	hide()
